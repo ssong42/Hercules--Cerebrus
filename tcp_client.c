8c152da7be39fe225c3171dfe36b2e845e800603
Steven Song <ssong@e1z2r7p30.42.us.org>
2018-03-03 09:43:49 -0800

    norminetted and working

--- tcp_client.c
+++ tcp_client.c
@@ -6,7 +6,7 @@
 /*   By: ssong <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 15:45:14 by ssong             #+#    #+#             */
-/*   Updated: 2018/03/02 18:14:05 by ssong            ###   ########.fr       */
+/*   Updated: 2018/03/03 09:24:23 by ssong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-// netinet contains a structure so we know the address we want to connect to
 
 void	ft_putchar(char c)
 {
@@ -31,35 +30,28 @@ void	ft_putstr(char *str)
 	}
 }
 
-int main()
+int		main(int argc, char **argv)
 {
-	int	 network_socket;
+	int					network_socket;
+	struct sockaddr_in	server_address;
+	int					connect_status;
+	char				server_response[256];
 
-	// create a socket
+	if (argc != 2)
+	{
+		ft_putstr("usage: message to client\n");
+		return (0);
+	}
 	network_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-	// specify an address for the socket
-	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
-	// htons converts the port address to appropriate data format for struct
 	server_address.sin_port = htons(9002);
-	// sin_addr is a struct that holds data of the address we are trying to connect to
-	// INADDR_ANY is same as connecting to 0.0.0.0
 	server_address.sin_addr.s_addr = INADDR_ANY;
-	// creates the connection between socket and server address
-	int connect_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
-	// connect returns integer whether the connection occured
+	connect_status = connect(network_socket, (struct sockaddr *)
+			&server_address, sizeof(server_address));
 	if (connect_status == -1)
 		ft_putstr("There was an error in connection to remote socket\n");
-
-	// recieve data from the server
-	char server_response[256];
-	char my_message[256] = "ping";
-
-	send(network_socket, my_message, sizeof(my_message), 0);
+	send(network_socket, argv[1], sizeof(argv[1]), 0);
 	recv(network_socket, &server_response, sizeof(server_response), 0);
-
-	// print out the server's response
 	ft_putstr(server_response);
 	ft_putchar('\n');
 	close(network_socket);
--- tcp_server.c
+++ tcp_server.c
@@ -6,12 +6,11 @@
 /*   By: ssong <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 16:34:34 by ssong             #+#    #+#             */
-/*   Updated: 2018/03/02 18:12:55 by ssong            ###   ########.fr       */
+/*   Updated: 2018/03/03 09:43:23 by ssong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -30,7 +29,7 @@ void	ft_putstr(char *str)
 	}
 }
 
-int	ft_strcmp(const char *s1, const char *s2)
+int		ft_strcmp(const char *s1, const char *s2)
 {
 	int i;
 
@@ -42,47 +41,28 @@ int	ft_strcmp(const char *s1, const char *s2)
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
-int main()
+int		main(void)
 {
-	char server_message[256] = "Pong\nPong";
-	char error_message[256] = "NOT PING";
-	// create server socket
-	int server_socket;
+	int					server_socket;
+	struct sockaddr_in	server_address;
+	int					client_socket;
+	char				client_message[256];
+
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-	// define the server address
-	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
-	
-	// htons converts the port address to appropriate data format for struct
 	server_address.sin_port = htons(9002);
-	
-	// sin_addr is a struct that holds data of the address we are trying to connect to
-	// INADDR_ANY is the ip address of local machine
 	server_address.sin_addr.s_addr = INADDR_ANY;
-
-	// bind the socket to our specified IP and port
-	bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-
-	listen(server_socket, 42);
-
-	int	client_socket;
-	// second parament of accept can store the information of the client into a struct (PORT, IP);
-	client_socket = accept(server_socket, NULL, NULL);
-
-	char client_message[256];
-	char test[6] = "ping";
-
-	recv(client_socket, &client_message, sizeof(client_message), 0);
-	ft_putstr(client_message);
-	ft_putchar('\n');
-	if (ft_strcmp(client_message, test) == 0)
+	bind(server_socket, (struct sockaddr*)
+			&server_address, sizeof(server_address));
+	while (ft_strcmp(client_message, "ping"))
 	{
-		// send the message
-		send(client_socket, server_message, sizeof(server_message), 0);
+		listen(server_socket, 42);
+		client_socket = accept(server_socket, NULL, NULL);
+		recv(client_socket, &client_message, sizeof(client_message), 0);
+		if (ft_strcmp(client_message, "ping"))
+			send(client_socket, "Send me ping", sizeof(client_message), 0);
 	}
-	else
-		send(client_socket, error_message, sizeof(error_message), 0);
+	send(client_socket, "Pong\nPong", sizeof(client_message), 0);
 	close(server_socket);
 	return (0);
 }
